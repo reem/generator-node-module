662cdc97b880a9cb3bf8cef6b6fdbce0eb3ab58e
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-01 16:53:13 -0700

    (bug) Fixed typo in test template.

--- app/templates/_tests/_index.js
+++ app/templates/_tests/_index.js
@@ -1,7 +1,7 @@
 var <%= moduleVar %> = require('../lib');
 var demand = require('must');
 
-define('<%= moduleName %>', function () {
+describe('<%= moduleName %>', function () {
   it('should exist', function () {
     demand(<%= moduleVar %>).to.exist();
   });
