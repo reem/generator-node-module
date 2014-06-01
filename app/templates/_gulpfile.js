07b205694bc5939fc58b8b7cb992678e3f0cd3d1
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-01 16:41:08 -0700

    (bug) Fixed jshint reporter.

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var jshint = require('gulp-jshint');
+var gulp    = require('gulp');
+var mocha   = require('gulp-mocha');
+var jshint  = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('lint', function () {
   'use strict';
   return gulp.src('./lib/**/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
 });
 
